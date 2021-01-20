b5d30f774a57871e6c733ff59954d50117d8fafb
Hiranmaya Gundu <hiranmayagundu@gmail.com>
2021-01-20 16:22:57 +0530

    push collage

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import Box from "@material-ui/core/Box";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import { Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,7 +37,7 @@ export default function ActualHome() {
       <div className={classes.bgWrap}>
         <Image
           alt="Mountains"
-          src="/images/mountains_placehoder.jpg"
+          src="/images/sona_collage.jpg"
           layout="fill"
           objectFit="cover"
           quality={100}
@@ -48,11 +49,16 @@ export default function ActualHome() {
         container
         spacing={4}
         justify="center"
+        alignItems="center"
       >
-        <Grid item xs={12}>
-          <Typography variant="h1" color="textSecondary" gutterBottom>
-            Happy Birthday Suhas!
-          </Typography>
+        <Grid item xs={12} justify="center">
+          <Box width={500} ml="auto" mr="auto">
+            <Paper>
+              <Typography variant="h3" color="textSecondary" gutterBottom>
+                Happy Birthday Suhas!
+              </Typography>
+            </Paper>
+          </Box>
         </Grid>
         <Grid item>
           <Link href="/this-or-that">
